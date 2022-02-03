cc2080cbbf185df0d210141c38f87cd50961878f
Orlov <vladislav_orlov_94@mail.ru>
2022-02-03 21:29:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 20;
+        int percent = 5;
+
+        int mil = ticketPrice * percent / 100;
+
+//        int ticketPrice = 100;
+//        int mil = ticketPrice /20;
+
+
+        System.out.println("Бонус в милях за перелет " + mil);
+
+    }
+
+}
+
